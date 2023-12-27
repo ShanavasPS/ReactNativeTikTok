0d4d4b048a8bd174088de8860379135a0b947d4b
Shanavas Shaji <shanavascruise@gmail.com>
2023-12-27 21:48:24 +0200

    Updated tab bar color

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,3 +1,4 @@
+import { Image } from "react-native"
 import Activity from "../screens/Activity"
 import Bookmarks from "../screens/Bookmarks"
 import Discover from "../screens/Discover"
@@ -9,7 +10,36 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: { backgroundColor: 'black' }, // Set the background color of the tab bar
+        tabBarActiveTintColor: 'white', // Set the active tab text color
+        tabBarLabelStyle: {
+          color: 'white',
+          fontSize: 10,
+          fontFamily: 'SF Pro Rounded',
+          fontWeight: '500',
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconSource;
+
+          if (route.name === 'Home') {
+            iconSource = require('../assets/home.png');
+          } else if (route.name === 'Discover') {
+            iconSource = require('../assets/discover.png');
+          } else if (route.name === 'Activity') {
+            iconSource = require('../assets/activity.png');
+          } else if (route.name === 'Bookmarks') {
+            iconSource = require('../assets/bookmarks.png');
+          } else if (route.name === 'Profile') {
+            iconSource = require('../assets/profile.png');
+          }
+
+          return <Image source={iconSource} style={{ tintColor: color, width: size, height: size }} />;
+        },
+        })}
+      >
           <Tab.Screen 
             name="Home" 
             component={Home}
