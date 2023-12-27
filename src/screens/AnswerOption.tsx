b788e8cf028a193cec815e5a3c8b0d0043285efb
Shanavas Shaji <shanavascruise@gmail.com>
2023-12-27 22:35:37 +0200

    Fixed the background color of the options

--- src/screens/AnswerOption.tsx
+++ src/screens/AnswerOption.tsx
@@ -10,16 +10,16 @@ const AnswerOption = ({title}: ItemProps) => {
   };
 
   return (
-    <View>
-        <TouchableOpacity onPress={onPress} style={[styles.container]}>
-          <View style={styles.container}>
-            <Text style={styles.optionText}>{title}</Text>
-            <Image source={require('../assets/cross.png')} />
-          </View>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress} style={styles.touchableOpacity}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.optionText}>{title}</Text>
+          <Image style={styles.iconImage} source={require('../assets/cross.png')} />
+        </View>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -29,27 +29,32 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     flexDirection: 'row',
-    gap: 10,
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+  },
+  touchableOpacity: {
+    flex: 1,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
   },
   optionText: {
     color: 'white',
     fontSize: 17,
-    padding: 12,
     fontFamily: 'SF Pro Rounded',
     fontWeight: '500',
+    paddingRight: 12,
+    flex: 1,
     flexWrap: 'wrap',
     textShadowColor: 'rgba(0, 0, 0, 0.45)',
     textShadowOffset: { width: 1, height: 1.5 },
     textShadowRadius: 2,
   },
-      iconContainer: {
-        marginLeft: 8,
-      },
-      innerContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      },
-})
+  iconImage: {
+  },
+});
+
 
 export default AnswerOption
