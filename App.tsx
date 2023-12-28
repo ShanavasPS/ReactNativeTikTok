b9fe45f23660dd1f2c70951857283ab57619d38b
Shanavas Shaji <shanavascruise@gmail.com>
2023-12-28 14:01:50 +0200

    Status bar is translucent now

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './src/navigators/TabNavigator';
 import FloatingActionButtons from './src/screens/FloatingActionButtons';
 import { store } from './src/store/data_store';
+import { Platform, StatusBar } from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    StatusBar.setBarStyle("light-content");
+    if (Platform.OS === "android") {
+      StatusBar.setBackgroundColor("rgba(0,0,0,0)");
+      StatusBar.setTranslucent(true);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
